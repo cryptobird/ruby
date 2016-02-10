b920b4c2f0659a4d939946ed38e874a016c7e5be
Jon Zingale <jon@jackrabbitsystems.com>
2016-02-10 13:03:07 -0700

    added a byebug

--- qsort.rb
+++ qsort.rb
@@ -1,10 +1,16 @@
+require 'byebug'
+
 list = [*1..100]
 list.shuffle!
 
 def qksort(arr)
   piv = arr.pop
+byebug
   left, right = arr.partition{|x| x < piv}
   return (qksort(left) << piv) + qksort(right)
 end
 
 qksort(list)
+
+
+nil
