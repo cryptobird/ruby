90d43d5ee1492e8582c236825ead17cd4cbc9b1f
cryptobird <parrott@Parrottbook.local>
2016-02-11 13:27:56 -0700

    finished qksort implimentation

--- qsort.rb
+++ qsort.rb
@@ -1,16 +1,15 @@
-require 'byebug'
+def qksort(arr)
+  if arr.nil? || arr.empty? then [] else
+    piv = arr.pop
+    left, right = arr.partition{|x| x < piv}
+    return (qksort(left) << piv) + qksort(right)
+  end
+end
 
 list = [*1..100]
 list.shuffle!
-
-def qksort(arr)
-  piv = arr.pop
-byebug
-  left, right = arr.partition{|x| x < piv}
-  return (qksort(left) << piv) + qksort(right)
-end
-
-qksort(list)
+puts list
+puts qksort(list)
 
 
 nil
